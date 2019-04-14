071fcc54711cd53b1843782e8b7ae195c023426c
Miracle Anyanwu <anyanwumiracle78@gmail.com>
2019-04-14 12:37:40 +0100

    switched var to let

--- src/lib.js
+++ src/lib.js
@@ -1,30 +1,34 @@
-const axios = require('axios');
-const crypto = require('crypto');
-const version = 'npm-opennode-v1.1.0';
+const axios = require("axios");
+const crypto = require("crypto");
+const version = "npm-opennode-v1.1.0";
 var instance = undefined;
 var api_key;
 var env;
 
-
-function setCredentials(key = '', environment = 'live') {
+function setCredentials(key = "", environment = "live") {
   if (instance != undefined) return;
 
   api_key = key;
   instance = axios.create();
   env = environment;
-  instance.defaults.baseURL = (environment === 'live') ? 'https://api.opennode.co/v1' : 'https://dev-api.opennode.co/v1';
+  instance.defaults.baseURL =
+    environment === "live"
+      ? "https://api.opennode.co/v1"
+      : "https://dev-api.opennode.co/v1";
   instance.defaults.timeout = 15000;
-  instance.defaults.headers = { 'Authorization' : api_key, 'user_agent' : version };
+  instance.defaults.headers = { Authorization: api_key, user_agent: version };
 }
 
 async function createCharge(charge) {
   try {
-
     const response = await instance.post(`/charges`, charge);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
@@ -32,76 +36,94 @@ async function chargeInfo(id) {
   try {
     const response = await instance.get(`/charge/${id}`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function listCharges() {
   try {
-
     const response = await instance.get(`/charges`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function initiateWithdrawal(withdrawal) {
   try {
-    console.warn(`This method is deprecated and not recommend for use. Please use the asynchronous version (initiateWithdrawalAsync)`);
+    console.warn(
+      `This method is deprecated and not recommend for use. Please use the asynchronous version (initiateWithdrawalAsync)`
+    );
 
     const response = await instance.post(`/withdrawals`, withdrawal);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function withdrawalInfo(id) {
   try {
-
     const response = await instance.get(`/withdrawal/${id}`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function listWithdrawals() {
   try {
-
     const response = await instance.get(`/withdrawals`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function listRates() {
   try {
-
     const response = await instance.get(`/rates`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function listCurrencies() {
   try {
-
     const response = await instance.get(`/currencies`);
     return response.data.data;
-
   } catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
@@ -109,40 +131,54 @@ async function userBalance() {
   try {
     const response = await instance.get(`/account/balance`);
     return response.data.data;
-  }
-  catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+  } catch (error) {
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function initiateWithdrawalAsync(withdrawal) {
   try {
     let new_instance = axios.create();
-    new_instance.defaults.baseURL = (env === 'live') ? 'https://api.opennode.co/v2' : 'https://dev-api.opennode.co/v2';
+    new_instance.defaults.baseURL =
+      env === "live"
+        ? "https://api.opennode.co/v2"
+        : "https://dev-api.opennode.co/v2";
     new_instance.defaults.timeout = 15000;
-    new_instance.defaults.headers = { 'Authorization' : api_key, 'user_agent' : version };
+    new_instance.defaults.headers = {
+      Authorization: api_key,
+      user_agent: version
+    };
 
-    const response = await new_instance.post('/withdrawals', withdrawal);
+    const response = await new_instance.post("/withdrawals", withdrawal);
     return response.data.data;
-  }
-  catch (error) {
-    throw Exception(error.response.status, error.response.statusText, error.response.data.message);
+  } catch (error) {
+    throw Exception(
+      error.response.status,
+      error.response.statusText,
+      error.response.data.message
+    );
   }
 }
 
 async function verifySignature(charge) {
-  const hash = crypto.createHmac('sha256', api_key).update(charge.id).digest('hex');
+  const hash = crypto
+    .createHmac("sha256", api_key)
+    .update(charge.id)
+    .digest("hex");
   return hash === charge.hashed_order;
 }
 
 function Exception(statusCode, statusText, message) {
-  var error = new Error(message);
+  let error = new Error(message);
   error.name = statusText;
   error.status = statusCode;
 
   return error;
 }
-
 
 module.exports = {
   setCredentials: setCredentials,
@@ -156,6 +192,5 @@ module.exports = {
   listRates: listRates,
   listCurrencies: listCurrencies,
   signatureIsValid: verifySignature,
-  getBalance: userBalance,
+  getBalance: userBalance
 };
-
